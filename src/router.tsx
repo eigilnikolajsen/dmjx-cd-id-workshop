de6c470783bc5fb7cd4af456da1105e7fdf48028
eigilnikolajsen <eigi0088@gmail.com>
2026-05-21 09:29:49 +0200

    fix: router crash

--- src/router.tsx
+++ src/router.tsx
@@ -2,21 +2,21 @@ import { createRouter as createTanStackRouter } from "@tanstack/solid-router";
 import { PendingComponent } from "./components/pending-component";
 import { routeTree } from "./routeTree.gen";
 
-const router = createTanStackRouter({
-	routeTree,
-	scrollRestoration: true,
-	defaultStaleTime: 10 * 1000, // 10 seconds
-	defaultPreloadStaleTime: 30 * 1000, // 30 seconds
-	defaultGcTime: 30 * 60 * 1000, // 30 minutes
-	defaultPendingComponent: PendingComponent,
-	defaultPreload: "render",
-});
-
-const getRouter = (): typeof router => router;
+// oxlint-disable-next-line explicit-module-boundary-types, explicit-function-return-type
+const getRouter = () =>
+	createTanStackRouter({
+		routeTree,
+		scrollRestoration: true,
+		defaultStaleTime: 10 * 1000, // 10 seconds
+		defaultPreloadStaleTime: 30 * 1000, // 30 seconds
+		defaultGcTime: 30 * 60 * 1000, // 30 minutes
+		defaultPendingComponent: PendingComponent,
+		defaultPreload: "render",
+	});
 
 declare module "@tanstack/solid-router" {
 	interface Register {
-		router: typeof router;
+		router: ReturnType<typeof getRouter>;
 	}
 }
 
